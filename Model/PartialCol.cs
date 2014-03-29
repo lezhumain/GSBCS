091791b64663426b32b19112e478e2967e71c736
lezhumain <julien.merion@epsi.fr>
2014-03-29 19:36:18 +0100

    modele MVVM ameliore ; liste des collaborateurs

---
+++ Model/PartialCol.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Objects.DataClasses;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public partial class COLLABORATEUR : EntityObject
+    {
+        public override string ToString()
+        {
+            return "<COLLABORATEUR>" + this.prenom_col + " " + this.nom_col;
+        }
+    }
+}
--- WpfApplication/ViewModel/MainWindowViewModel.cs
+++ WpfApplication/ViewModel/MainWindowViewModel.cs
@@ -11,13 +11,53 @@ using System.Windows.Media;
 using System.Runtime.CompilerServices;
 using WpfApplication.Helpers;
 using System.Windows.Input;
+using Model;
 
 namespace WpfApplication.ViewModel
 {
     public class MainWindowViewModel : MainViewModel
     {
         private readonly IServiceClient serviceClient;
+        
+        /* Commandes */
         private DelegateCommand exitCommand;
+        public ICommand ExitCommand
+        {
+            get
+            {
+                if (exitCommand == null)
+                    exitCommand = new DelegateCommand(Exit);
+
+                return exitCommand;
+            }
+        }
+        
+        private DelegateCommand testCommand;
+        public ICommand TestCommand
+        {
+            get
+            {
+                if (testCommand == null)
+                    testCommand = new DelegateCommand(Test);
+
+                return testCommand;
+            }
+        }
+
+        /*
+        private DelegateCommand voirPratCommand;
+        public ICommand VoirPratCommand
+        {
+            get
+            {
+                if (voirPratCommand == null)
+                    voirPratCommand = new DelegateCommand(VoirPrat);
+
+                return voirPratCommand;
+            }
+        }
+         */
+        /*************/
 
         private string prenom;
         public string Prenom
@@ -41,6 +81,37 @@ namespace WpfApplication.ViewModel
             set { NotifyPropertyChanged(ref listeClients, value); }
         }
 
+        /* Entites */
+        private List<COLLABORATEUR> listeCol;
+        public List<COLLABORATEUR> ListeCol
+        {
+            get { return listeCol; }
+            set { NotifyPropertyChanged(ref listeCol, value); }
+        }
+        /*
+        private List<RAPPORT_DE_VISITE> listeRap;
+        public List<RAPPORT_DE_VISITE> ListeRap
+        {
+            get { return listeRap; }
+            set { NotifyPropertyChanged(ref listeRap, value); }
+        }
+
+        private List<VISITEUR> listeVis;
+        public List<VISITEUR> ListeVis
+        {
+            get { return listeVis; }
+            set { NotifyPropertyChanged(ref listeVis, value); }
+        }
+
+        private List<PRATICIEN> listePra;
+        public List<PRATICIEN> ListePra
+        {
+            get { return listePra; }
+            set { NotifyPropertyChanged(ref listePra, value); }
+        }
+         */
+        /***********/
+
         private SolidColorBrush bonClient;
         public SolidColorBrush BonClient
         {
@@ -63,6 +134,12 @@ namespace WpfApplication.ViewModel
 
             Client client = serviceClient.Charger();
             ListeClients = service.ChargerTout();
+            ListeCol = ColHelper.Current.GetList().ToList<COLLABORATEUR>();
+            /*
+            ListeVis = null;
+            ListeRap = null;
+            ListePra = null;
+             */
 
             Prenom = client.Prenom;
             Age = client.Age;
@@ -72,20 +149,23 @@ namespace WpfApplication.ViewModel
                 BonClient = new SolidColorBrush(Color.FromArgb(100, 255, 0, 0));
         }
 
-        public ICommand ExitCommand
-        {
-            get
-            {
-                if (exitCommand == null)
-                    exitCommand = new DelegateCommand(Exit);
-
-                return exitCommand;
-            }
-        }
-
         private void Exit()
         {
             Application.Current.Shutdown();
         }
+
+        private void Test()
+        {
+            COLLABORATEUR c = listeCol[0];
+
+            MessageBox.Show(c.ToString());
+        }
+
+        /*
+        private void VoirPrat()
+        {
+
+        }
+         */
     }
 }
